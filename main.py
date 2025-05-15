47858487af634cc288673a12a1c6f9dc98bad677
aghabidareh <aghabidareh@gmail.com>
2025-05-15 20:37:04 +0330

    dashboard-step-one

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import plotly.express as px
 import plotly.graph_objects as go
 from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
+
+# Loading and preparing data
+df = pd.read_csv('superstore.csv')
+df['Order Date'] = pd.to_datetime(df['Order Date'])
+df['Year'] = df['Order Date'].dt.year
+
