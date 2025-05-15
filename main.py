a8714c6b4a3f34844bb2ec42fe6a746415cbc8ae
aghabidareh <aghabidareh@gmail.com>
2025-05-15 20:39:50 +0330

    dashboard-step-three

--- main.py
+++ main.py
@@ -12,4 +12,15 @@ df['Year'] = df['Order Date'].dt.year
 # Dash with Dark Theme
 app = Dash(__name__, external_stylesheets=[dbc.themes.LITERA])
 
+# Defining custom styles
+SIDEBAR_STYLE = {
+    'position': 'fixed',
+    'top': 0.0,
+    'left': 0.0,
+    'bottom': 0.0,
+    'width': "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#1a1a1a",
+    "box-shadow": "2px 0 5px rgba(0,0,0,0.5)"
+}
 
